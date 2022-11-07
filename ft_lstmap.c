32d7b4c795d2d31e1a8df266d9d18d0311c82006
Max Pankewitz <max@firemail.eu>
2022-11-07 15:42:03 +0100

    lstmap strict working, all strict working

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,26 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpankewi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mpankewi <mpankewi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 17:17:17 by mpankewi          #+#    #+#             */
-/*   Updated: 2022/11/05 16:45:41 by mpankewi         ###   ########.fr       */
+/*   Updated: 2022/11/07 15:39:46 by mpankewi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*nnode;
 	t_list	*nlist;
+	void	*tmp;
 
 	nlist = NULL;
+	if (f == NULL || del == NULL || lst == NULL)
+		return (0);
 	while (lst)
 	{
-		nnode = ft_lstnew(f(lst->content));
+		tmp = f(lst->content);
+		nnode = ft_lstnew(tmp);
 		if (!nnode)
 		{
-			ft_lstclear(&nnode, del);
+			free(tmp);
+			ft_lstclear(&nlist, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&nlist, nnode);
