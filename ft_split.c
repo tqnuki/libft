5e8cb281fca11765a8eac011d45818285c317702
Max Pankewitz <max@firemail.eu>
2022-11-07 15:22:43 +0100

    split working strict, free all

--- ft_split.c
+++ ft_split.c
@@ -3,31 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpankewi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mpankewi <mpankewi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 13:44:39 by mpankewi          #+#    #+#             */
-/*   Updated: 2022/11/05 16:22:55 by mpankewi         ###   ########.fr       */
+/*   Updated: 2022/11/07 15:21:41 by mpankewi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-static int	wordcount(const char *s, char c)
+static int	wordcount(const char *s, char c, int opt, char **strs)
 {
 	int	i;
 	int	count;
 
 	i = 0;
 	count = 0;
-	while (s[i])
+	if (opt == 1)
 	{
-		while (s[i] && s[i] == c)
-			i++;
-		if (s[i] && s[i] != c)
-			count++;
-		while (s[i] && s[i] != c)
-			i++;
+		while (s[i])
+		{
+			while (s[i] && s[i] == c)
+				i++;
+			if (s[i] && s[i] != c)
+				count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		return (count);
 	}
-	return (count);
+	while (strs[i])
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+	return (1);
 }
 
 static int	wordlen(const char *s, char c, int i)
@@ -50,8 +61,12 @@ static char	*putword(const char *str, char charset, int i)
 	int		j;
 
 	j = 0;
+	if (!str)
+		return (NULL);
 	len = wordlen(str, charset, i);
 	rep = malloc(sizeof(char) * (len + 1));
+	if (!rep)
+		return (NULL);
 	while (str[i] && str[i] != charset)
 	{
 		rep[j] = str[i];
@@ -59,6 +74,32 @@ static char	*putword(const char *str, char charset, int i)
 		i++;
 	}
 	rep[j] = '\0';
+	return (rep);
+}
+
+static char	**split2(const char *str, char charset, int j, int i)
+{
+	char	**rep;
+
+	rep = NULL;
+	rep = malloc(sizeof(char *) * (wordcount(str, charset, 1, rep) + 1));
+	if (!rep)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		while (str[i] != '\0' && str[i] == charset)
+			i++;
+		if (str[i] != '\0')
+		{
+			rep[j] = putword(str, charset, i);
+			if (!rep[j++])
+				if (wordcount(str, charset, 0, rep))
+					return (NULL);
+		}
+		while (str[i] && str[i] != charset)
+			i++;
+	}
+	rep[j] = 0;
 	return (rep);
 }
 
@@ -72,18 +113,9 @@ char	**ft_split(const char *str, char charset)
 	j = 0;
 	if (!str)
 		return (NULL);
-	rep = malloc(sizeof(char *) * (wordcount(str, charset) + 1));
+	rep = NULL;
+	rep = split2(str, charset, j, i);
 	if (!rep)
 		return (NULL);
-	while (str[i] != '\0')
-	{
-		while (str[i] != '\0' && str[i] == charset)
-			i++;
-		if (str[i] != '\0')
-			rep[j++] = putword(str, charset, i);
-		while (str[i] && str[i] != charset)
-			i++;
-	}
-	rep[j] = 0;
 	return (rep);
 }
